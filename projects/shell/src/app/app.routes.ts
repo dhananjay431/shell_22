f1c253904fb3df826f31d0f0bc05e01f907d3492
dbel431 <dbel431@gmail.com>
2026-08-26 09:45:14 +0530

    change route

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { loadRemoteModule } from '@angular-architects/native-federation';
 
 export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
   {
     path: '',
     loadComponent: () => import('./home.component').then((m) => m.HomeComponent),
